29c9891e4359c42664903e20e40b9c2242b1169f
vanity1412 <vvthong.insec@gmail.com>
2026-06-07 16:30:08 +0700

    Align Android JVM target

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,6 +14,15 @@ android {
         versionCode = 1
         versionName = "1.0.0"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 dependencies {
